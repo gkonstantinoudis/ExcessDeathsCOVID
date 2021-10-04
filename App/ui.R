49659a41c3e6e626024386becea8c6683f7772c7
Virgilio Gómez-Rubio <virgilio.gomez@uclm.es>
2021-10-04 22:48:41 +0200

    code clenup

--- App/ui.R
+++ App/ui.R
@@ -19,10 +19,8 @@ if(system("hostname", intern = TRUE) == "atlasmortalidad.uclm.es") {
 # Header con logo
 dbHeader <- dashboardHeader(title = "EXCESS MORTALITY APP",
  tags$li(a(href = '', #'https://www.uclm.es',
-   #img(src = 'Logo_uclm.png',
    img(src = '',
-   #title = "Universidad de Castilla-La Mancha", height = "30px"),
-   title = "", height = "30px"),
+   title = "Excess Mortality App", height = "30px"),
    style = "padding-top:10px; padding-bottom:10px;"),
    class = "dropdown"
  )
@@ -96,23 +94,6 @@ tags$style(type = "text/css", "#leafletexcess {height: calc(100vh - 110px) !impo
               type = 6, color = "#990033")
         )
       ),
-#      tabItem(tabName = "sppprob",
-#        fluidPage(
-##https://stackoverflow.com/questions/36469631/how-to-get-leaflet-for-r-use-100-of-shiny-dashboard-height
-#          h4("Posterior probabilities"),
-#tags$style(type = "text/css", "#leafletpprob {height: calc(100vh - 110px) !important;}"),
-#            shinycssloaders::withSpinner(leafletOutput("leafletpprob"),
-#              type = 6, color = "#990033")
-#        )
-#      ),
-#      tabItem(tabName = "tasas",
-#        fluidPage(
-#          h4(uiOutput("titulotasas")),
-#          tags$style(type = "text/css", "#leaflettasas {height: calc(100vh - 110px) !important;}"),
-#            shinycssloaders::withSpinner(leafletOutput("leaflettasas"),
-#              type = 6, color = "#990033")
-#        )
-#      ),
       tabItem(tabName = "temporal", 
         fluidPage(
           h4("Excess Mortality"),
@@ -120,16 +101,6 @@ tags$style(type = "text/css", "#leafletexcess {height: calc(100vh - 110px) !impo
           shinycssloaders::withSpinner(leafletOutput("leaflettseries"),
             type = 6, color = "#990033"),
         )),
-#      tabItem(tabName = "Rt",
-#        fluidPage(
-#          h4(uiOutput("tituloRt")),
-#          #h4("Número reproductivo básico instantaneo"),
-#          h5(strong("Pincha en un municipio para ver la evolución del número reproductivo básico instantaneo")),
-#          tags$style(type = "text/css", "#leafletRt {height: calc(100vh - 230px) !important;}"),
-#          shinycssloaders::withSpinner(leafletOutput("leafletRt"),
-#            type = 6, color = "#990033"),
-#        )),
-#
      tabItem(tabName = "tmpsummary",
        fluidPage(
        h4("Summary (national level)"),
@@ -170,7 +141,6 @@ tags$style(type = "text/css", "#leafletexcess {height: calc(100vh - 110px) !impo
      tabItem(tabName = "stsummary",
        fluidPage(
        h4("Summary"),
-         #dataTableOutput('tabspsummary')
          tableOutput('tabstsummary'),
          fluidPage(includeMarkdown("legend.Rmd")) 
        )
@@ -184,89 +154,6 @@ tags$style(type = "text/css", "#leafletexcess {height: calc(100vh - 110px) !impo
          fluidPage(includeMarkdown("legend_spatiotemp.Rmd"))
          )
      ),
-#     tabItem(tabName = "ine",
-#       fluidPage(
-#         h4(uiOutput("tituloINE")),
-#         h5(strong("Datos de estimación de defunciones semanales (Instituto Nacional de Estadística)")),
-#         plotlyOutput("ineplot"),
-#           fluidRow(
-#             selectInput("provinciaine", "Provincia", c("Todas", "Albacete", "Ciudad Real", "Cuenca", "Guadalajara", "Toledo"),
-#               choices = list(Todas = "Todas", Albacete = "AB", "Ciudad Real" = "CR", Cuenca = "CU", Guadalajara = "GU", Toledo = "TO")),
-#             selectInput("sexoine", "Sexo", list(Todos = "Todos", Hombres = "Hombre", Mujeres = "Mujer"),
-#               selected = "Todos"),
-#             selectInput("gedadine", "Grupo de edad",
-#                c("Todos", "Edad < 65", "Edad 65-74", "Edad > 75"),
-#                selected = "Todos"),
-#           )
-#         )
-#     ),
-
-#     tabItem(tabName = "rebrote",
-#       fluidPage(
-#         h4(uiOutput("titulorebrote")),
-#tags$style(type = "text/css", "#leafletrebrote {height: calc(100vh - 230px) !important;}"),
-#         shinycssloaders::withSpinner(leafletOutput("leafletrebrote"),
-#            type = 6, color = "#990033")
-#       )
-#     ),
-
-
-#      tabItem(tabName = "umbral",
-#        fluidPage(
-#          h4(uiOutput("tituloumbral")),
-#tags$style(type = "text/css", "#leafletumbral {height: calc(100vh - 230px) !important;}"),
-#          shinycssloaders::withSpinner(leafletOutput("leafletumbral"),
-#            type = 6, color = "#990033"),
-#          br(),
-#          sliderInput("sliderumbral",
-#            label = "Umbral de confinamiento (IA14 por 100.000 habs.)",
-#            width = "325px",
-#            value = 500,
-#            min = 0,
-#            max = 2000,
-#            step = 10,
-#            animate = FALSE)
-#        )
-#      ),
-#      tabItem(tabName = "temporalcasos",
-#        fluidPage(
-#          h4(uiOutput("titulotemporalcasos")),
-#tags$style(type = "text/css", "#leaflettemporalcasos {height: calc(100vh - 230px) !important;}"),
-#          shinycssloaders::withSpinner(leafletOutput("leaflettemporalcasos"),
-#            type = 6, color = "#990033"),
-#          br(),
-#          sliderInput("slidersemanacasos",
-#            label = "Semana",
-#            value = 34,
-#            min = 34,
-#            max = ultima.semana,
-#            step = 1,
-#            animate = animationOptions(interval = 8000))
-#        )
-#      ),
-#      tabItem(tabName = "temporaltasas",
-#        fluidPage(
-#          h4(uiOutput("titulotemporaltasas")),
-#tags$style(type = "text/css", "#leaflettemporaltasas {height: calc(100vh - 230px) !important;}"),
-#          shinycssloaders::withSpinner(leafletOutput("leaflettemporaltasas"),
-#            type = 6, color = "#990033"),
-#          br(),
-#          sliderInput("slidersemanatasas",
-#            label = "Semana",
-#            value = 34,
-#            min = 34,
-#            max = ultima.semana,
-#            step = 1,
-#            animate = animationOptions(interval = 8000))
-#        )
-#      ),
-#      tabItem(tabName = "observatorio",
-#        fluidRow(
-#          htmlOutput("obscovid19ab"),
-#          tags$style(type = "text/css", "#ABOBSiframe {height: calc(100vh - 230px) !important;}")
-#        )
-#      ),
-      #tabItem(tabName = "help", fluidPage(withMathJax(includeMarkdown("help.Rmd")))),
       tabItem(tabName = "contact", fluidPage(includeMarkdown("contact.Rmd")))
 
     )
